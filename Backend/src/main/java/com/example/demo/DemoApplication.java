945d3d06d35651bb051202a65154856a0d6fcac7
유수민 <proysm5154@naver.com>
2023-11-06 21:37:08 +0900

    Fix build error, nothing now

--- Backend/src/main/java/com/example/demo/DemoApplication.java
+++ Backend/src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication // (exclude = {DataSourceAutoConfiguration.class})
 public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
