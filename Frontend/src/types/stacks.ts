218142059f443f0df93085e4d7ecd3e81800bb62
유수민 <proysm5154@naver.com>
2023-11-25 20:14:22 +0900

    Update: 전체 기술 스택 저장할  stack.ts 생성

---
+++ Frontend/src/types/stacks.ts
@@ -0,0 +1,10 @@
+export const stacks = [
+    { value: '1', label: 'React' },
+    { value: '2', label: 'Next.js' },
+    // 나머지 옵션들...
+    { value: '99', label: 'SpringBoot' },
+    { value: '100', label: 'Node.js' },
+];
+
+// Knowledge Graph의 모든 노드(stack) 추가할 예정
+  
