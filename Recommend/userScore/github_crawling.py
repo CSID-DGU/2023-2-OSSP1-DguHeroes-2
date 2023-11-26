c8686d6538b2bcac4c8d7bc4393a0a32873003ee
dgu2022 <ccccavin12345@naver.com>
2023-11-27 04:27:03 +0900

    Feat: 깃허브 크롤링 업데이트 #3

--- Recommend/userScore/github_crawling.py
+++ Recommend/userScore/github_crawling.py
@@ -1,13 +1,10 @@
 import requests
 import json
 
-GITHUB_API_TOKEN = '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'
-OWNER = 'proysm'
-REPO = 'Healody_Server'
-# OWNER='CSID-DGU'
-# REPO='2023-2-OSSP1-DguHeroes-2'
+NAME = 'proysm/Healody_Server'
+# NAME='CSID-DGU/2023-2-OSSP1-DguHeroes-2'
 
-MAX_PER_PAGE = 30  # 용량을 줄이기 위해 테스톨 30, 실제로는 100
+MAX_PER_PAGE = 100  # 용량을 줄이기 위해 테스톨 30, 실제로는 100
 
 GH_API = 'https://api.github.com'
 
@@ -15,23 +12,36 @@ headers = {
     'Authorization': 'token %s' % (GITHUB_API_TOKEN),
 }
 
-GRAPH_LANGUAGE = ['JavaScript', 'HTML', 'CSS', 'Python', 'TypeScript', 'Java', 'C#', 'C++', 'C', 'PHP', 'Go', 'Rust',
-                  'Kotlin' \
-    , 'Ruby', 'Lua', 'Dart', 'Swift', 'R', 'Visual Basic']
+GRAPH_STACK_TREE = {
+    'javascript': ['express', 'socket.io', 'axios', 'react', 'angularjs', 'react native', 'electron', 'vue.js', \
+                   'jquery', 'next.js', 'svelte'], 'html': ['electron'], 'css': [],
+    'python': ['flask', 'django', 'pandas', 'numpy', \
+               'tensorflow', 'scikit-learn', 'apache kafka', 'pytorch', 'opencv', 'opengl', 'keras', 'apache spark',
+               'qt'], \
+    'typescript': ['react', 'angular', 'react native'], 'java': ['springboot', 'Apache Kafka'], 'c#': ['blazor', \
+                                                                                                       '.net',
+                                                                                                       '.net framework'],
+    'c++': ['cuda', 'apache kafka'], 'c': ['apache kafka', 'cuda'], 'php': ['laravel'], \
+    'go': ['apache kafka'], 'rust': [], 'kotlin': [], 'ruby': ['ruby on rails'], 'lua': [], 'dart': [], 'swift': [], \
+    'r': [], 'visual basic': ['.net', '.net framework'], 'node.js': ['express', 'socket.io', 'axios', 'electron'], \
+    'flutter': [], '.net': ['blazor'], 'rabbitmq': []}
+
+GRAPH_LANGUAGE = ['javascript', 'html', 'css', 'python', 'typescript', 'java', 'c#', 'c++', 'c', 'php', 'go', 'rust',
+                  'kotlin' \
+    , 'ruby', 'lua', 'dart', 'swift', 'r', 'visual basic']
+''' #혹시 쓰일지도 모를, 대문자용 리스트
+GRAPH_LANGUAGE = ['JavaScript', 'HTML', 'CSS', 'Python', 'TypeScript', 'Java', 'C#', 'C++', 'C', 'PHP', 'Go', 'Rust', 'Kotlin'\
+               , 'Ruby', 'Lua', 'Dart',  'Swift', 'R', 'Visual Basic']
+'''
 list_language_extension = [['js'], ['html'], ['css'], ['py'], ['ts'], ['java', 'class', 'jsp'], ['cs'],
                            ['cc', 'cpp', 'h'] \
     , ['c', 'h'], ['php'], ['go'], ['rs'], ['kt'], ['rb'], ['lua'], ['dart'], ['s'], ['swift'], ['r'], ['vb']]
 
 
-# Project ID 구하는 함수
-def get_project_id():
-    return 0
-
-
 # Project 사용 언어 구하는 함수
 def get_language():
     # https://api.github.com/repos/OWNER/REPO/languages
-    GH_REPO = '%s/repos/%s/%s/languages' % (GH_API, OWNER, REPO)
+    GH_REPO = '%s/repos/%s/languages' % (GH_API, NAME)
     response = requests.get('%s' % (GH_REPO), headers=headers)
     response = response.json()
     list_language = list(response.keys())
@@ -47,7 +57,7 @@ def get_members():
     # collaborators가 아니면서 contributors기만 한 팀원도 있을 수 있다고 생각해 그냥 배제하기로 했다. 대신 contributors에서 필터링.
 
     #  https://api.github.com/repos/OWNER/REPO/contributors
-    GH_REPO = '%s/repos/%s/%s/contributors' % (GH_API, OWNER, REPO)
+    GH_REPO = '%s/repos/%s/contributors' % (GH_API, NAME)
     min_rate_commits = 0.05;  # 상대적-5퍼센트 이상은 돼야 팀원으로 인정
     min_cnt_commits = 10;  # 절대적-10개 이상은 돼야 팀원으로 인정
     response = requests.get('%s' % (GH_REPO), headers=headers)
@@ -70,10 +80,12 @@ def get_members():
     return list_name_members, list_cnt_commits
 
 
-# Commit Code 가져오는 함수
+# Commit Code 가져오는 함수 + 추가한 라인 수도 카운트한 뒤 합해서 전달 (깃허브 점수 2번 구하기 위해)
 def get_commit_code(list_furl):
     list_addition_code = []
     list_filename_language = []
+    cnt_addition = 0
+    cnt_deletion = 0
     for furl in list_furl:
         response = requests.get('%s' % (furl), headers=headers)
         response = response.json()
@@ -88,10 +100,11 @@ def get_commit_code(list_furl):
                 # list_addition_code.append(response['files'][idx_file]['additions'])
                 filename = response['files'][idx_file]['filename'].split('.')[-1]
                 list_filename_language.append(filename)
-                print(response['files'][idx_file])
+                cnt_addition += response['files'][idx_file]['additions']
+                cnt_deletion += response['files'][idx_file]['deletions']
                 list_addition_code.append(response['files'][idx_file]['patch'])
 
-    return list_addition_code, list_filename_language
+    return list_addition_code, list_filename_language, cnt_addition, cnt_deletion
 
 
 # Commit SHA값 포함된 주소 가져오는 함수
@@ -99,9 +112,8 @@ def get_commit_sha(fauthor, fsum):
     # max_page_num = int(fsum+99/100) #실제
     max_page_num = 1  # 용량 줄이려고 테스트용
     list_url_commits = []
-    for page in range(max_page_num):
-        GH_REPO = '%s/repos/%s/%s/commits?per_page=%s&page=%s&author=%s' % (
-        GH_API, OWNER, REPO, MAX_PER_PAGE, page, fauthor)
+    for page in range(1, max_page_num + 1):
+        GH_REPO = '%s/repos/%s/commits?per_page=%s&page=%s&author=%s' % (GH_API, NAME, MAX_PER_PAGE, page, fauthor)
 
         response = requests.get('%s' % (GH_REPO), headers=headers)
         response = response.json()
@@ -127,17 +139,7 @@ dict_user_commit = dict()
 list_language = get_language()
 print(list_language)
 
-project_id = get_project_id()
 list_name_members, list_cnt_commits = get_members()
 
 print(list_cnt_commits)
 print(list_name_members)
-
-list_name_members = ['rlJzr', 'twosky0202']
-
-for idx, member in enumerate(list_name_members):
-    list_url_commit = get_commit_sha(member, list_cnt_commits[idx])
-    list_commit_code, list_filename = get_commit_code(list_url_commit)
-    dict_user_commit[member] = {'code': [], 'code_language': [], 'stack': []}
-    dict_user_commit[member]['code'] = list_commit_code  # 멤버별로 커밋 코드를 포함하는 딕셔너리 생성하기
-    dict_user_commit[member]['stack'] = [list_filename]
