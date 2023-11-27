7230138e4e33e6f71ad88bbeff5b245506a46de6
dgu2022 <ccccavin12345@naver.com>
2023-11-27 16:58:36 +0900

    Feat: 깃허브 크롤링 업데이트 #4

--- Recommend/userScore/get_score_usability.py
+++ Recommend/userScore/get_score_usability.py
@@ -1,6 +1,13 @@
+import requests
+import json
+import numpy as np
+import base64
+import time
+
+
 # 깃허브 점수 3번 활용량 구하는 함수들
-# 프로젝트 issue 유무 측정하는 함수:
-def get_cnt_issue():
+# 프로젝트 issue 개수 측정하는 함수:
+def get_cnt_issue(NAME):
     # https://api.github.com/repos/OWNER/REPO/issues
     GH_REPO = '%s/search/issues?q=repo:%s+type:issue' % (GH_API, NAME)
 
@@ -17,7 +24,7 @@ def get_cnt_issue():
 
 
 # 프로젝트 branch 개수 측정하는 함수:
-def get_cnt_branch():
+def get_cnt_branch(NAME):
     # https://api.github.com/repos/OWNER/REPO/branches
     GH_REPO = '%s/repos/%s/branches' % (GH_API, NAME)
 
@@ -34,7 +41,7 @@ def get_cnt_branch():
 
 
 # 프로젝트 pr 개수 측정하는 함수:
-def get_cnt_pr():
+def get_cnt_pr(NAME):
     # https://api.github.com/repos/OWNER/REPO/pull
     GH_REPO = '%s/search/issues?q=repo:%s+type:pr' % (GH_API, NAME)
 
@@ -51,7 +58,7 @@ def get_cnt_pr():
 
 
 # 프로젝트 tag 개수 측정하는 함수:
-def get_cnt_tag():
+def get_cnt_tag(NAME):
     # https://api.github.com/repos/OWNER/REPO/tags
     GH_REPO = '%s/repos/%s/tags' % (GH_API, NAME)
 
@@ -69,7 +76,7 @@ def get_cnt_tag():
 
 
 # 프로젝트 release 개수 측정하는 함수:
-def get_cnt_release():
+def get_cnt_release(NAME):
     # https://api.github.com/repos/OWNER/REPO/releases
     GH_REPO = '%s/repos/%s/releases' % (GH_API, NAME)
 
@@ -87,16 +94,11 @@ def get_cnt_release():
 
 
 # 프로젝트 깃허브 활용도 구하는 함수:
-def get_score_usability():
-    tag = get_cnt_tag()
-    release = get_cnt_release()
-    pr = get_cnt_pr()
-    branch = get_cnt_branch()
-    issue = get_cnt_issue()
+def get_cnt_usability(NAME):
+    tag = get_cnt_tag(NAME)
+    release = get_cnt_release(NAME)
+    pr = get_cnt_pr(NAME)
+    branch = get_cnt_branch(NAME)
+    issue = get_cnt_issue(NAME)
 
-    score_usability = tag + release + pr + branch + issue
-
-    return score_usability
-
-
-print(get_score_usability())
+    return tag, release, pr, branch, issue
