9ef51dfd2572cbe31df7b47436dd3bdf393b58fb
유수민 <proysm5154@naver.com>
2023-12-06 18:55:01 +0900

    Feat : project position

---
+++ Backend/src/main/java/com/example/demo/domain/position/ProjectEtc.java
@@ -0,0 +1,14 @@
+package com.example.demo.domain.position;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProjectEtc extends PositionBase {
+
+}
