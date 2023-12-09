46774a9d92ba501406bc5a044fdbb3d1f733b0b0
Seonghyeok Im <happyim0321@gmail.com>
2023-12-09 15:40:37 +0900

    feat: 알고리즘에 거리 계산 연결
    calculateurs에서 getDistance method를 사용하도록 반영함

--- Recommend/matchingAlgorithm/GetDistance.java
+++ Recommend/matchingAlgorithm/GetDistance.java
@@ -6,12 +6,13 @@ import org.neo4j.driver.Result;
 import org.neo4j.driver.Session;
 import org.neo4j.driver.Value;
 
+//neo4j 그래프 db로부터 노드간 최단거리를 가져오는 함수
 public class GetDistance {
-  public static double getDistance(String stack1, String stack2){
+  public static float getDistance(String stack1, String stack2){
     String uri = "neo4j+s://1fc435f2.databases.neo4j.io";
     String user = "neo4j";
     String password = "hgjtCyWMi5hVUIHhc0Au_VLVcOGkxBe0FM02DqUoX7k";
-    double distance=0;
+    float distance=0;
 
     try (Driver driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
         Session session = driver.session()) {
@@ -27,7 +28,7 @@ public class GetDistance {
       Record record = result.next();
       Value totalWeight = record.get("totalWeight");
       System.out.println("Total Weight: " + totalWeight.asDouble());
-      distance=totalWeight.asDouble();
+      distance=totalWeight.asFloat();
 
     } catch (Exception e) {
       e.printStackTrace();
--- Recommend/matchingAlgorithm/urs.java
+++ Recommend/matchingAlgorithm/urs.java
@@ -26,12 +26,12 @@ public class urs {
     // 특정 프로젝트에서 특정 유저의 userRoleScore를 계산해주는 메소드
     static float calculateUrs(int projId, int userId, String targetStack){
         float urs = 0;
+        float distance=0;
 
         //구현: 프로젝트(projId)가 요구하는 기술스택 리스트를 PROJECT_SPEC 테이블에서 추출 (중복 제거) -----
         List<String> projStack = new ArrayList<>();
         projStack.add("Java");
         projStack.add("SpringBoot");
-        projStack.add("React");
         //----------------------------------------------------------------------------------
 
         //구현: 유저(userId)가 사용할 줄 아는 기술스택 리스트를 USER_STACK 테이블에서 추출 -----------
@@ -42,13 +42,19 @@ public class urs {
 
         List<String> intersection = findIntersection(projStack, userStack);
 
-        //System.out.println(intersection + " : intersection success");
-
-        //구현: intersection을 이용하여 참조해야 할 숙련도 테이블들 선택 후,
-        //선택된 숙련도 테이블들과 함수의 input으로 받은 stack, 그리고 STACK_DISTANCE 테이블을 가지고 userRoleScore 계산 --------
-        Random random = new Random();
-        urs = 3 + random.nextFloat() * (10 - 3);
-        //--------------------------------------------------------------------------------------------------------
+        for(int i=0;i<intersection.size();i++){
+            String element=intersection.get(i);
+            if(targetStack.equals(element)){
+                distance=0;
+            }else{
+                distance=GetDistance.getDistance(targetStack, element);
+            }
+            // random.nextFlot()*10을 ursscore(element)로 바꾸면 된다.
+            // element에 해당하는 stack을 db에서 읽어와서 점수를 호출하면 됨.
+            Random random = new Random();
+            //선택된 숙련도 테이블들과 함수의 input으로 받은 stack, 그리고 그래프db 반환값을 가지고 userRoleScore 계산
+            urs+=3/(3+distance)*random.nextFloat()*10;
+        }
 
         return urs;
     }
