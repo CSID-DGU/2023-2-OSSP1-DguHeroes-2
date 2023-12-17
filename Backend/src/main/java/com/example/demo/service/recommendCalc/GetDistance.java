1aace68ccb539dd8ad6fd33171e6c566fa2af41f
Seonghyeok Im <happyim0321@gmail.com>
2023-12-18 05:25:50 +0900

    feat: 프로젝트 기준 추천 구현

--- Backend/src/main/java/com/example/demo/service/recommendCalc/GetDistance.java
+++ Backend/src/main/java/com/example/demo/service/recommendCalc/GetDistance.java
@@ -1,45 +1,48 @@
 package com.example.demo.service.recommendCalc;
 
-import org.neo4j.driver.*;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
-
-import java.lang.Record;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Record;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Session;
 
 @Service
 public class GetDistance {
+
     @Value("${security.neo4j.uri}")
-    String neo4jUri;
+    private static String neo4jUri;
 
     @Value("${security.neo4j.user}")
-    String neo4jUser;
+    private static String neo4jUser;
 
     @Value("${security.neo4j.password}")
-    String neo4jPassword;
+    private static String neo4jPassword;
 
-//    public static float getDistance(String stack1, String stack2){
-//
-//        float distance=0;
-//
-//        try (Driver driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
-//             Session session = driver.session()) {
-//            String start="MATCH path=allShortestPaths((startNode:techStack {techStack: '";
-//            String middle="'})-[:relation*..10]-(endNode:techStack {techStack: '";
-//            String end="'}))" +
-//                    "RETURN REDUCE(s = 0, rel IN relationships(path) | s + rel.weight) AS totalWeight";
-//
-//            String query=start+stack1+middle+stack2+end;
-//
-//            Result result = session.run(query);
-//
-//            Record record = result.next();
-//            Value totalWeight = record.get("totalWeight");
-//            System.out.println("Total Weight: " + totalWeight.asDouble());
-//            distance=totalWeight.asFloat();
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return distance;
-//    }
+    public static float getDistance(String stack1, String stack2){
+
+        float distance=0;
+
+        try (Driver driver = GraphDatabase.driver(neo4jUri, AuthTokens.basic(neo4jUser, neo4jPassword));
+             Session session = driver.session()) {
+            String start="MATCH path=allShortestPaths((startNode:techStack {techStack: '";
+            String middle="'})-[:relation*..10]-(endNode:techStack {techStack: '";
+            String end="'}))" +
+                    "RETURN REDUCE(s = 0, rel IN relationships(path) | s + rel.weight) AS totalWeight";
+
+            String query=start+stack1+middle+stack2+end;
+
+            Result result = session.run(query);
+
+            Record record = result.next();
+            org.neo4j.driver.Value totalWeight =  record.get("totalWeight");
+            distance=totalWeight.asFloat();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return distance;
+    }
 }
